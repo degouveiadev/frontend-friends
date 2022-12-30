17014147a593ce6e5064e6b7f2ee287e1d78c62d
Juan Rodriguez <juanjo.webdeveloper@gmail.com>
2022-12-30 09:54:46 -0400

    fix(ui): set default theme

--- packages/ui/src/ui-provider/ui-provider.tsx
+++ packages/ui/src/ui-provider/ui-provider.tsx
@@ -1,17 +1,13 @@
 import { FC, ReactNode } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import {
-  extendTheme,
-  withDefaultColorScheme,
-  ThemingProps,
-} from '@chakra-ui/react';
+import { extendTheme, ThemingProps } from '@chakra-ui/react';
 
 type UiProviderProps = {
   children: ReactNode | ReactNode[];
-  theme: ThemingProps;
+  theme?: ThemingProps;
 };
 
-const themeBase = extendTheme(withDefaultColorScheme);
+const themeBase = extendTheme({});
 
 export const UiProvider: FC<UiProviderProps> = ({ children, theme }) => {
   return <ChakraProvider theme={theme || themeBase}>{children}</ChakraProvider>;
